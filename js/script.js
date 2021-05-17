83fca3b031584771206f4f7b78ed3731fc13f7d6
Stefano-Para <stefano.zina@gmail.com>
2021-05-17 16:31:06 +0200

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -8,23 +8,39 @@ var app = new Vue(
 
         el: '#root',
         data: {
-            message: 'Hello Vue JS!',
+            message: 'Hai voglia di fare due passi?',
+            buttonText: "Vai via dalla città",
             colorClass: 'red',
+            imgSrc: "https://www.turismo-oggi.com/wp-content/uploads/2018/02/Genova-770x405.jpg"
             },
-        methods: {
-            colorChange: function () {
+            methods: {
+                colorChange: function () {
 
-                if (this.colorClass = "red") {
-                    this.colorClass = "blue";
-                }
-                else if (this.colorClass = "blue") {
-                    this.colorClass = "green";
-                } else {
-                    this.colorClass = "red";
+                    if (this.colorClass == "red") {
+                        this.colorClass = "blue";
+                    }
+                    else if (this.colorClass == "blue") {
+                        this.colorClass = "green";
+                    } else {
+                        this.colorClass = "red";
+                    }
+                },
+            
+                imgChange: function () {
+                    if (this.imgSrc == "https://www.turismo-oggi.com/wp-content/uploads/2018/02/Genova-770x405.jpg") {
+
+                        this.imgSrc = "https://media.springernature.com/lw630/nature-cms/uploads/cms/pages/4855/top_item_image/NClimate_Hero_MAY-21-6c5764f2b141511b4f3a7f71f670376a.jpg";
+                        this.buttonText = "fai un altro tentativo";
+
+                    } else if (this.imgSrc == "https://media.springernature.com/lw630/nature-cms/uploads/cms/pages/4855/top_item_image/NClimate_Hero_MAY-21-6c5764f2b141511b4f3a7f71f670376a.jpg") {
+                        this.imgSrc = "https://static.wikia.nocookie.net/lotr/images/b/b0/Fotr6.jpg/revision/latest?cb=20130419144638";
+                        this.buttonText = "Alla fine la città non è così male..";
+                    } else {
+                        this.imgSrc = "https://www.turismo-oggi.com/wp-content/uploads/2018/02/Genova-770x405.jpg"
+                        this.buttonText = "Vai via dalla città"
+                    }
                 }
             }
-        }
-        
     }
 );
 
